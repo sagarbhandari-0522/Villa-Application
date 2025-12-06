8aa4ebde6e60e0c3713195a8e1747e5684dc4c13
Sagar Bhandari <sagarvandari1@gmail.com>
2025-12-07 12:32:11 +1300

    Database setup

---
+++ Villa.Web/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private ApplicationDbContext _context;
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
