375bf9dede487b633fe9d1cedd39b8cb4b58d448
Sagar Bhandari <sagarvandari1@gmail.com>
2025-12-08 14:38:09 +1300

    feat: create villa

--- Villa.Web/Controllers/VillaController.cs
+++ Villa.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Villa.Domain.Entities;
 using Villa.Infrastructure.Data;
 
 namespace Villa.Web.Controllers
@@ -14,6 +15,26 @@ namespace Villa.Web.Controllers
         {
             var villas = _context.Villas.ToList();
             return View(villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Room obj)
+        {
+            if(obj.Name==obj.Description)
+            {
+                ModelState.AddModelError("Name", "Name and Description should not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
         }
     }
 }
