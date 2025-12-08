c684e05f413f7ffd5388b58aaef7eb898bb87e8e
Sagar Bhandari <sagarvandari1@gmail.com>
2025-12-08 16:24:48 +1300

    feat: villa update and delete action

--- Villa.Web/Controllers/VillaController.cs
+++ Villa.Web/Controllers/VillaController.cs
@@ -36,5 +36,56 @@ namespace Villa.Web.Controllers
             return View(obj);
 
         }
+        public IActionResult Update(int villaId)
+        {
+            
+            var villaInfo = _context.Villas.FirstOrDefault(v => v.Id == villaId);
+            if(villaInfo is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villaInfo);
+        }
+        [HttpPost]
+        public IActionResult Update(Room obj)
+        {
+            if(obj.Name==obj.Description)
+            {
+                ModelState.AddModelError("Name", "Name and Description can't be same");
+            }
+            if(ModelState.IsValid)
+            {
+                _context.Villas.Update(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+                return View(obj);
+        }
+        [HttpGet]
+        public IActionResult Delete(int villaId)
+        {
+            var villaObj = _context.Villas.FirstOrDefault(v => v.Id == villaId);
+            if(villaObj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(villaObj);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var villaObj = _context.Villas.FirstOrDefault(v => v.Id == id);
+            if(villaObj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+           _context.Villas.Remove(villaObj);
+           _context.SaveChanges();
+            return RedirectToAction("Index");
+          
+
+        }
+
     }
 }
