59c3809446870dca1d0834e8cb3c2dbb3e5da8c7
Sagar Bhandari <sagarvandari1@gmail.com>
2025-12-17 12:44:41 +1300

    feat: add room repository

---
+++ Villa.Infrastructure/Repository/RoomRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Application.Interface;
+using Villa.Domain.Entities;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Infrastructure.Repository
+{
+    public class RoomRepository : IRoomRepository
+    {
+        public readonly ApplicationDbContext _context;
+        public RoomRepository(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+        public void Add(Room room)
+        {
+            _context.Villas.Add(room);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Room> GetAll(Expression<Func<Room, bool>>? filter = null, params Expression<Func<Room, object>>[] includeProperties)
+        {
+
+            IQueryable<Room> query = _context.Villas;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties != null)
+            {
+
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.ToList();
+        }
+
+        public Room GetById(int id, params Expression<Func<Room, object>>[] includeProperties)
+        {
+            IQueryable<Room> query = _context.Villas;
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.FirstOrDefault(v => v.Id == id);
+        }
+
+        public void Remove(Room room)
+        {
+            _context.Villas.Remove(room);
+            _context.SaveChanges();
+
+        }
+
+        public void Update(Room room)
+        {
+            _context.Villas.Update(room);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Villa.application/Interface/IRoomRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Domain.Entities;
+
+namespace Villa.Application.Interface
+{
+    public interface IRoomRepository
+    {
+        IEnumerable<Room> GetAll(Expression<Func<Room, bool>>? filter = null, params Expression<Func<Room, object>>[] includeProperties);
+        Room GetById(int id, params Expression<Func<Room, object>>[] includeProperties);
+        void Add(Room room);
+        void Remove(Room room);
+        void Update(Room room);
+
+    }
+}
