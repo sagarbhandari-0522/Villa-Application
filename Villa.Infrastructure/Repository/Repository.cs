b97c79b056ebf40252b958088f2d1c902c0f1da9
Sagar Bhandari <sagarvandari1@gmail.com>
2025-12-19 15:46:27 +1300

    feat: add general repository

---
+++ Villa.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Application.Interface;
+using Villa.Domain.Entities;
+using Villa.Infrastructure.Data;
+
+namespace Villa.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AnyFilter(Expression<Func<T, bool>> filter)
+        {
+            if(filter==null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+          
+               return _context.Set<T>().Any(filter);
+        
+           
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, params Expression<Func<T, object>>[] includeProperties)
+        {
+
+            IQueryable<T> query = _context.Set<T>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties != null)
+            {
+
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.ToList();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> filter, params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (includeProperties != null)
+            {
+                foreach (var includeProperty in includeProperties)
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+            return query.FirstOrDefault(filter);
+        }
+
+
+    }
+}
---
+++ Villa.application/Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Villa.Domain.Entities;
+
+namespace Villa.Application.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter=null, params Expression<Func<T, object>>[] includeProperties);
+        T GetSingle(Expression<Func<T,bool>> filter, params Expression<Func<T, object>>[] includeProperties);
+        bool AnyFilter(Expression<Func<T, bool>> filter);
+      
+    }
+}
